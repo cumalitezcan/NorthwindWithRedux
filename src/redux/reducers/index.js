8d8f05ff9f93c9417c3e95c9ba81026d28ab26ad
cumalitezcan <cumalitezcan@gmail.com>
2020-11-12 15:38:23 +0300

    creating store

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,10 @@
+import {combineReducers} from 'redux'
+import changeCategoryReducer from './changeCategoryReducer'
+
+const rootReducer = combineReducers({
+/*     changeCategoryReducer:changeCatehoryReducer
+ */
+        changeCategoryReducer
+})
+
+export default rootReducer;
