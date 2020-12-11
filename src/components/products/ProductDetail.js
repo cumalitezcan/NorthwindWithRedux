abf1920f262b4ba66e8cbd31a2deec002f6bcf60
cumalitezcan <cumalitezcan@gmail.com>
2020-12-11 18:42:09 +0300

    create SelectInput

--- src/components/products/ProductDetail.js
+++ src/components/products/ProductDetail.js
@@ -1,22 +1,34 @@
 import React from 'react'
+import SelectInput from '../toolbox/SelectInput';
 import TextInput from '../toolbox/TextInput'
 
-const ProductDetail = (
+
+const ProductDetail = ({
     categories,
     product,
     onSave,
     onChange
 
-) => {
+}) => {
     return (
         <form onSubmit={onSave}>
             <h2>{product.id? "Güncelle" : "Ekle"}</h2>
             <TextInput name="productName" label="Product Name" value={product.productName} onChange={onChange} error="Hata"/>
 
+            <SelectInput name="categoryId" label="Category" value={product.categoryId || ""} defaultOption="Seçiniz"
+             options={categories.map(category=>({
+                 value:category.id,
+                 text:category.categoryName
+             }))}
+                 onChange={onChange}
+                 error="Hata"
+             />
+
         <button type="submit" className="btn btn-success">Save</button>
 
         </form>
     )
+    
 };
 
 export default ProductDetail;
